85c21c1aaebd8b9a903f47815de324c9d8f6ed1b
Akshat Tewari <akshattewari4@gmail.com>
2026-09-25 21:37:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (60.06%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        bool seen[26] = {false};
+
+        for (char c : sentence) {
+            seen[c - 'a'] = true;
+        }
+
+        for (bool b : seen) {
+            if (!b) return false;
+        }
+
+        return true;
+    }
+};
