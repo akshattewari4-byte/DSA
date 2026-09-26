f8e20bfa6b587ef1eb06d1ab83793b2d603a463b
Akshat Tewari <akshattewari4@gmail.com>
2026-09-26 18:37:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (12.90%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -11,14 +11,31 @@
  */
 class Solution {
 public:
+
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p==NULL||q==NULL){
-            return p==q;
+
+        if (p == NULL || q == NULL) {
+            return p == q;
         }
-        bool isleft=isSameTree(p->left,q->left);
-        bool isright=isSameTree(p->right,q->right);
+
+        bool isleft = isSameTree(p->left, q->left);
+        bool isright = isSameTree(p->right, q->right);
 
         return isleft && isright && p->val == q->val;
-        
+    }
+
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+
+        if (root == NULL || subRoot == NULL) {
+            return root == subRoot;
+        }
+
+        if (root->val == subRoot->val &&
+            isSameTree(root, subRoot)) {
+            return true;
+        }
+
+        return isSubtree(root->left, subRoot) ||
+               isSubtree(root->right, subRoot);
     }
 };
